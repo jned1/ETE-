7b155e970aec9c73ba157b7e68782951fcc655d5
jned1 <jaffarjned12@gmail.com>
2025-11-27 10:05:17 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Hardcoded user
+const user = {
+  username: "admin",
+  password: "1234"
+};
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === user.username && password === user.password) {
+    return res.json({
+      success: true,
+      message: "Login successful!"
+    });
+  }
+
+  return res.json({
+    success: false,
+    message: "Invalid credentials"
+  });
+});
+
+app.listen(3000, () => console.log("Login project running on 3000"));
